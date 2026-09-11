3482d018fb52e9048d7b2e557f4a892350909669
vinod <vinodkotte94@gmail.com>
2026-09-11 22:16:25 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -23,6 +23,7 @@ origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://netshield-ai-frontend-fpbu.onrender.com",
+    "https://net-shield-ai-azure.vercel.app",
 ]
 
 
